f172aa0fe974b08577622a6d68e4862a6641960d
Dmitry Bakhvalov <dizzus@gmail.com>
2015-07-19 15:36:08 +0300

    Initial Commit

---
+++ PromiseKit/PromiseKit.h
@@ -0,0 +1,19 @@
+//
+//  PromiseKit.h
+//  PromiseKit
+//
+//  Created by Дмитрий Бахвалов on 19.07.15.
+//  Copyright (c) 2015 Dmitry Bakhvalov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PromiseKit.
+FOUNDATION_EXPORT double PromiseKitVersionNumber;
+
+//! Project version string for PromiseKit.
+FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseKit/PublicHeader.h>
+
+
